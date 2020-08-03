aebf18794fb463e58a2f86ccb9467aaac62b8da7
Никита Сторожук <nikitastorozuk@MacBook-Air-Nikita.local>
2020-08-03 21:38:18 +0300

    Finish 6 lesson

--- nodejs-start/express.js
+++ nodejs-start/express.js
@@ -1,98 +1,133 @@
 const express = require('express')
 const consolidate = require('consolidate')
-const path = require('path')
+const path = require('path') 
+const mongoose = require('mongoose') // для работы с базой
+var config = require('./config') // настройки
 
-const app = express()
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
 
-const users = {
-    anna: {
-        username: 'Anna',
-        age: 27,
-        skills: [
-            'JS', 'Node.js', 'Express.js',
-        ],
-    },
-    ivan: {
-        username: 'Ivan',
-        skills: [
-            'TypeScript', 'Node.js',
-        ],
-    },
-}
+const app = express() // создаем приложение
+
+const taskModel = require('./models/task'); // подключаем модель
+const userModel = require('./models/user'); // подключаем модель User
+const passport = require('./auth') // подключаем модуль авторизации
+
+mongoose.connect(`mongodb://127.0.0.1:${config.mongoosePort}/todo`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}) // соединение с базой
 
 // какой шаблонизатор используется
 app.engine('hbs', consolidate.handlebars)
-// какой формат используется
 app.set('view engine', 'hbs')
-// путь к папке с шаблонами
 app.set('views', path.resolve(__dirname, 'views'))
 
 //Middleware для работы с form
 app.use(express.urlencoded({extended: false}))
 
+app.use(session({
+    resave: true,
+    saveUninitialized: false,
+    secret: '1234',
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}))
+
+app.use(passport.initialize)
+app.use(passport.session)
+
+
 //Для работы с JSON
 app.use(express.json())
 
-//Пользовательские middleware
-app.use('/users', (req, res, next) => {
-    console.log('User middleware2! /users')
-    next()
-})
-
-//Срабатывает только на полное совпадение url
-app.all('/users', (req, res, next) => {
-    console.log('User middleware3! /users - app.all()')
-    next()
-})
-
-app.use((req, res, next) => {
-    //console.log(req.headers)
-    if(req.headers.test && req.headers.test === 'js'){
-        req.test = 'Test Ok! Middleware works'
-    }
-    next()
-})
-
-app.use((req, res, next) => {
-    console.log('User middleware!')
-    next()
-})
+// Проверка авторизации
+app.use('/tasks', passport.mustBeAutheticated)
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.redirect('/tasks')
+});
+
+// вывод задач
+app.get('/tasks', async (req, res) => {
+    const tasksObj = await taskModel.find({})
+    const tasks = JSON.parse(JSON.stringify(tasksObj))
+    res.render('tasks', {tasks})
 })
 
-app.get('/users', (req, res) => {
-    console.log(req.query)
-    res.send('Users!')
+// добавление задачи
+app.post('/tasks', async (req, res) => {
+    const task = new taskModel(req.body)
+    const isSaved = await task.save()
+    res.redirect('tasks')
 })
 
-app.post('/users', (req, res) => {
-    console.log(req.body)
-    console.log(req.body.name ? req.body.name : null)
+// редактирование задачи
+app.post('/tasks/update', async (req, res) => {
+    const { id, title } = req.body
 
-    console.log(req.test ? req.test : 'Заголовок test не был передан')
-
-    res.send('Post method works!')
+    await taskModel.updateOne({_id: id}, {$set: {title}})
+    res.redirect('/tasks')
 })
 
-//GET + params
-app.get('/users/:username', (req, res) => {
-    //console.log(req.params)
-    //res.send('GET + params works!')
-
-    const user = users[req.params.username] ? users[req.params.username] : users['anna']
-    res.render('user', user)  
+// задача завершена
+app.post('/tasks/complete', async (req, res) => {
+    const {id} = req.body
+    await taskModel.updateOne({_id: id}, {$set: { completed: true }})
+    res.redirect('/tasks')
 })
 
-//Для ДЗ
-app.post('/settings', (req, res) => {
-    console.log(req.body)
-    console.log(req.body.param1 ? req.body.param1 : 10)
-    res.render('user', {})
+// задача удалена
+app.post('/tasks/remove', async (req, res) => {
+    const {id} = req.body
+    await taskModel.findByIdAndRemove(id)
+    res.redirect('/tasks')
 })
 
-app.listen(4000, () => {
-    console.log('The server has been started!')
+// вывод одной задачи
+app.get('/tasks/:id', async(req, res) => {
+    const {id} = req.params
+    const task = await taskModel.findById(id)
+    res.render('task', task)  
+});
+
+
+// Registration Auth
+// Форма регистрации
+app.get('/register', (req, res) => {
+    res.render('register')
 })
+// Сама регистрация механика
+app.post('/register', async (req, res) => {
+    const { repassword, ...restBody } = req.body
+    if(restBody.password === repassword){
+        const user = new userModel(restBody)
+        await user.save()
+        res.redirect('/auth')
+    }else{
+        res.redirect('/register?err=repass')
+    }
+})
+
+// Форма авторизации
+app.get('/auth', (req, res) => {
+    const {error} = req.query
+    res.render('auth', {error})
+})
+
+// Авторизация
+app.post('/auth', passport.authenticate)
+
+// Выход
+app.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/auth')
+})
+
+
+// подключение статики для доступа с браузера
+app.use(express.static(path.join(__dirname, 'views', 'styles'))) 
+
+// запуск
+app.listen(config.webPort, () => console.log('=========== Server Start ==========='))
--- nodejs-start/models/task.js
+++ nodejs-start/models/task.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 
 const taskSchema = new Schema({
   title: {type: String, required: true},
+  completed: {type: Boolean, default: false},
   status: {type: String, default: 'to do'},
   priority: {type: String, default: 'normal'},
 })
